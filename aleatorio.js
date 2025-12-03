9a94ac0c3cb8ed188559cafab63af3be1365c955
gui dixava <141260850+AguinaldoRemes@users.noreply.github.com>
2025-12-03 14:13:19 -0300

    67890-

--- aleatorio.js
+++ aleatorio.js
@@ -1,6 +1,6 @@
 const nomes = ["Daiane", "lisiane", "masqueiquinho", "marlonildo ", "ratinho"];
 export function aleatorio (lista){
-    const posicao = Math.floor (Math.rondom()*lista.lenght);
+    const posicao = Math.floor (Math.random()*lista.lenght);
     return lista [posicao];
 }
 export const nome = aleatorio(nomes)
