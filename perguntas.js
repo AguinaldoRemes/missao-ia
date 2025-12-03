f37d91021d46e4e479b71a6742f974ef1ae8af3d
gui dixava <141260850+AguinaldoRemes@users.noreply.github.com>
2025-12-03 14:05:13 -0300

    98765

--- perguntas.js
+++ perguntas.js
@@ -5,14 +5,14 @@ export const perguntas = [
             {
                 texto: "O uso moderado de vapers pode ser uma alternativa menos prejudicial ao cigarro tradicional, contribuindo para a redução de danos relacionados ao fumo.",
                 afirmacao: [
-                    "Alguns estudos sugerem que os vapers são menos prejudiciais que os cigarros tradicionais, pois não envolvem a queima de tabaco, que libera substâncias carcinogênicas. No entanto, isso não significa que sejam completamente seguros, especialmente a longo prazo.",
+                    "Alguns estudos sugerem que os vapers são menos prejudiciais que os cigarros tradicionais, pois não envolvem a queima de tabaco, que libera substâncias carcinogênicas. No entanto, isso não significa que sejam completamente seguros, especialmente a longo prazo."
                 ],
                 proxima: 1,
             },
             {
                 texto: "O uso de *pods* pode causar dependência de nicotina, afetando negativamente a saúde mental e o desenvolvimento cognitivo de adolescentes.",
                 afirmacao: [
-                    "O uso de vapers pode levar à dependência de nicotina, o que afeta o cérebro em desenvolvimento, prejudicando a capacidade de tomar decisões, a regulação emocional e o foco. Isso é especialmente preocupante em jovens cujos cérebros ainda estão em formação.",
+                    "O uso de vapers pode levar à dependência de nicotina, o que afeta o cérebro em desenvolvimento, prejudicando a capacidade de tomar decisões, a regulação emocional e o foco. Isso é especialmente preocupante em jovens cujos cérebros ainda estão em formação."
                 ],
                 proxima: 2,
             }
@@ -24,14 +24,14 @@ export const perguntas = [
             {
                 texto: "Os sabores agradáveis tornam os vapers mais atraentes para os jovens, mas isso pode mascarar os riscos de dependência e problemas de saúde.",
                 afirmacao: [
-                    "A variedade de sabores disponíveis nos vapers pode atrair especialmente os jovens, criando uma falsa percepção de que são menos nocivos. No entanto, o consumo regular de nicotina através desses dispositivos pode levar a sérios problemas de saúde e dependência.",
+                    "A variedade de sabores disponíveis nos vapers pode atrair especialmente os jovens, criando uma falsa percepção de que são menos nocivos. No entanto, o consumo regular de nicotina através desses dispositivos pode levar a sérios problemas de saúde e dependência."
                 ],
                 proxima: 3,
             },
             {
                 texto: "A popularidade dos vapers entre os jovens é um reflexo de uma escolha informada e consciente sobre os riscos para a saúde.",
                 afirmacao: [
-                    "Na maioria dos casos, os jovens não estão completamente cientes dos riscos associados ao uso de vapers. A atração pelos sabores e a percepção de serem menos prejudiciais do que os cigarros tradicionais muitas vezes distorcem a realidade dos danos à saúde.",
+                    "Na maioria dos casos, os jovens não estão completamente cientes dos riscos associados ao uso de vapers. A atração pelos sabores e a percepção de serem menos prejudiciais do que os cigarros tradicionais muitas vezes distorcem a realidade dos danos à saúde."
                 ],
                 proxima: 4,
             }
@@ -43,14 +43,14 @@ export const perguntas = [
             {
                 texto: "O uso de vapers pode ser uma opção mais segura do que fumar cigarros tradicionais, pois não envolve combustão e a liberação de substâncias tóxicas.",
                 afirmacao: [
-                    "Os vapers não envolvem a queima de tabaco, o que pode reduzir a exposição a algumas substâncias cancerígenas presentes nos cigarros. No entanto, isso não elimina os riscos à saúde, já que o vapor pode conter substâncias que prejudicam a função pulmonar.",
+                    "Os vapers não envolvem a queima de tabaco, o que pode reduzir a exposição a algumas substâncias cancerígenas presentes nos cigarros. No entanto, isso não elimina os riscos à saúde, já que o vapor pode conter substâncias que prejudicam a função pulmonar."
                 ],
                 proxima: 5,
             },
             {
                 texto: "O uso de vapers pode prejudicar a saúde respiratória, causando inflamação nos pulmões e aumentando o risco de doenças pulmonares crônicas.",
                 afirmacao: [
-                    "Estudos demonstram que o uso de vapers pode causar danos aos pulmões, promovendo inflamação e aumentando o risco de doenças respiratórias, como bronquite crônica e outras condições relacionadas ao uso contínuo de nicotina e outros compostos presentes nos líquidos.",
+                    "Estudos demonstram que o uso de vapers pode causar danos aos pulmões, promovendo inflamação e aumentando o risco de doenças respiratórias, como bronquite crônica e outras condições relacionadas ao uso contínuo de nicotina e outros compostos presentes nos líquidos."
                 ],
                 proxima: 6,
             }
@@ -62,14 +62,14 @@ export const perguntas = [
             {
                 texto: "O uso de vapers oferece uma alternativa mais segura para quem quer parar de fumar, sem os mesmos riscos de doenças graves associadas ao cigarro tradicional.",
                 afirmacao: [
-                    "Embora os vapers possam ser menos prejudiciais em comparação ao cigarro tradicional, eles ainda apresentam riscos significativos, incluindo a dependência de nicotina, que pode afetar o desenvolvimento cerebral, emoções e tomada de decisões em jovens.",
+                    "Embora os vapers possam ser menos prejudiciais em comparação ao cigarro tradicional, eles ainda apresentam riscos significativos, incluindo a dependência de nicotina, que pode afetar o desenvolvimento cerebral, emoções e tomada de decisões em jovens."
                 ],
                 proxima: 7,
             },
             {
                 texto: "A dependência de nicotina provocada pelos vapers pode ter impactos sérios no desenvolvimento cognitivo e emocional dos adolescentes, prejudicando sua capacidade de aprender e regular suas emoções.",
                 afirmacao: [
-                    "A exposição à nicotina durante a adolescência pode interferir no desenvolvimento do cérebro, prejudicando funções cognitivas essenciais, como memória e tomada de decisões, além de aumentar o risco de distúrbios psicológicos, como ansiedade e depressão.",
+                    "A exposição à nicotina durante a adolescência pode interferir no desenvolvimento do cérebro, prejudicando funções cognitivas essenciais, como memória e tomada de decisões, além de aumentar o risco de distúrbios psicológicos, como ansiedade e depressão."
                 ],
                 proxima: 8,
             }
@@ -81,17 +81,17 @@ export const perguntas = [
             {
                 texto: "O uso de vapers pode ser uma alternativa socialmente aceitável em ambientes públicos, sem os mesmos estigmas e restrições do cigarro tradicional.",
                 afirmacao: [
-                    "Embora os vapers sejam vistos como uma alternativa mais 'limpa', o aumento do uso social pode normalizar o consumo de nicotina, levando a um maior número de jovens e adultos adotando hábitos prejudiciais à saúde.",
+                    "Embora os vapers sejam vistos como uma alternativa mais 'limpa', o aumento do uso social pode normalizar o consumo de nicotina, levando a um maior número de jovens e adultos adotando hábitos prejudiciais à saúde."
                 ],
                 proxima: 9,
             },
             {
                 texto: "O uso social de vapers pode ser prejudicial à saúde pública, pois pode incentivar o consumo de nicotina entre jovens, criando uma nova geração de dependentes.",
                 afirmacao: [
-                    "A popularização dos vapers pode contribuir para uma nova geração de dependentes de nicotina, pois o uso de dispositivos discretos e aromatizados facilita o consumo em ambientes sociais, tornando-o mais acessível e menos estigmatizado.",
+                    "A popularização dos vapers pode contribuir para uma nova geração de dependentes de nicotina, pois o uso de dispositivos discretos e aromatizados facilita o consumo em ambientes sociais, tornando-o mais acessível e menos estigmatizado."
                 ],
                 proxima: 10,
-            }
+            },
         ]
-    }
+    },
 ];
